f1c47b1331cc2e3631522e40bf1c249fe4072d4c
artyom <artem777mephi@GMAIL.COM>
2018-09-11 21:30:19 +0300

    Job complete

--- index.js
+++ index.js
@@ -1,41 +1,12 @@
-//1-й  вариант решения.Я только не понимаю как сделать,чтобы не конкатанацию делал а сложение
-var a =0;
-  function getsum(){
-
-var sum = prompt("Введите количество аргументов","***");
-for (n=0;n<sum;n++){
-
-    a+=prompt("Введите аргумент","")
-} 
-var b=a;
-return(b);
-}
-alert(getsum());
-//2-й вариант.Решение в лоб.
-
-function sum(a) {
-
-  return function(b) {
-     
-      return function(c){
-          return function(d){
-              return a+b+c+d
-          }
-      }
-  };
-
-}
-alert(sum(1)(2)(3)(4));
-//3-й вариант
 function sum (a){
-    var counter = a
+    var counter = a;
     function f(b){
-        counter+=b
-        return f
-    }
+        counter+=b;
+        return f;
+    };
     f.toString=function(){
-        return(counter)
-    }
-    return f
-}
-alert(sum(1)(2)(3)(4))
+        return(counter);
+    };
+    return f;
+};
+alert(sum(1)(2)(3)(4));
