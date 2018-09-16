e0117c1894c88cad0f98a83bc0c5d011df316c2d
artyom <artem777mephi@GMAIL.COM>
2018-09-16 22:49:37 +0300

    New commit

--- constructor.function.js
+++ constructor.function.js
@@ -1,8 +1,15 @@
-function Calculator(/** args */) {
-    // code here
+function Calculator() {
+    var args = Array.prototype.slice.call(arguments);
+     this.sum =function() {
+
+  return args.reduce(function(prev,cur){return prev+cur});
+     }
+    this.multiply=function(){
+        return args.reduce(function(prev,cur){return prev*cur});
+
+}
 }
 
-var calc = new Calculator(2, 3);
-
-alert(calc.multiply(2, 3)) // 6
-alert(calc.sum(2, 3)) // 5
+var calc = new Calculator(2,3,4,5);
+alert(calc.multiply()); // 140
+alert(calc.sum()); 
